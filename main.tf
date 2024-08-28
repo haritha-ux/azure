8f0e50f0cb0dd6db74a84e8e88245309051d0bb0
haritha-ux <sscit356@gmail.com>
2024-08-28 11:13:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,17 @@
-provider "azurerm" {
-  features = {}
-}
+# Create a azure resource group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-# output the resource group name
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+# Create a storage account
+resource "azurerm_storage_account" "storage" {
+  name                     = "${var.storage_account_name}${count.index}"
+  count                    = var.count_value
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = var.tags
 }
