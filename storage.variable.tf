836a218911589433862eeb4c3ae16b93e8362a62
haritha-ux <sscit356@gmail.com>
2024-08-28 10:26:44 +0530

    Update storage.variable.tf

--- storage.variable.tf
+++ storage.variable.tf
@@ -1,30 +1,21 @@
 variable "resource_group_name" {
   type        = string
-  description = "Name of the resourcegroup"
-  default     = "azurerm-terraform-group"
+
 }
 
 variable "location" {
   type    = string
-  default = "uksouth"
 }
 
 variable "storage_account_name" {
   type    = string
-  default = "devopsstorage23082024"
+
 }
 
 variable "tags" {
-  default = {
-    environment = "prod"
-    department  = "IT-Cloud"
-  }
+  
 }
-
-
 
 variable "count_value" {
   type    = number
-  default = 20
-
 }
