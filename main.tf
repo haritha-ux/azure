d9143a202692c015946d3c0cb189e11036eb8c60
haritha-ux <sscit356@gmail.com>
2024-08-28 10:58:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,11 @@
-provider "azurerm" {
-  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
-  features {}
-}
-#Create Resource group
+# main.tf
+
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
-#Create stroage account
-resource "azurerm_storage_account" "storage" {
 
-  name                     = lower(var.storage_account_name)
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-
-
+# output the resource group name
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
 }
